a3178821791cce0f45bb2cbc5bc76a6b12f4790b
huuphat1908 <huuphat1908@gmail.com>
2022-06-05 16:36:00 +0100

    add comprehensive skill test part 7

--- app/src/main/java/com/example/toeic_adventure/adapter/QuestionAdapter.java
+++ app/src/main/java/com/example/toeic_adventure/adapter/QuestionAdapter.java
@@ -6,18 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-
 import com.example.toeic_adventure.R;
-import com.example.toeic_adventure.model.Answer;
-import com.example.toeic_adventure.model.SkillTestList;
-
-import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class QuestionAdapter extends BaseAdapter {
@@ -25,16 +20,18 @@ public class QuestionAdapter extends BaseAdapter {
     int MyLayout;
     List<com.example.toeic_adventure.model.Question> QuestionList;
     List<com.example.toeic_adventure.model.Answer> AnswerList;
+    List<Boolean> IsSubmittedList;
 
     public QuestionAdapter(Context context,
                            int layout,
                            List<com.example.toeic_adventure.model.Question> questionList,
                            List<com.example.toeic_adventure.model.Answer> answerList,
-                           boolean isSubmitted) {
+                           List<Boolean> isSubmittedList) {
         MyContext = context;
         MyLayout = layout;
         QuestionList = questionList;
         AnswerList = answerList;
+        IsSubmittedList = isSubmittedList;
     }
 
     @Override
@@ -119,6 +116,64 @@ public class QuestionAdapter extends BaseAdapter {
                 AnswerList.get(i).userAnswer = rbD.getText().toString();
             }
         });
+        if (IsSubmittedList.get(i)) {
+            rgAnswer.setClickable(false);
+            rbA.setClickable(false);
+            rbB.setClickable(false);
+            rbC.setClickable(false);
+            rbD.setClickable(false);
+            rbA.setTextColor(MyContext.getResources().getColor(R.color.black));
+            rbB.setTextColor(MyContext.getResources().getColor(R.color.black));
+            rbC.setTextColor(MyContext.getResources().getColor(R.color.black));
+            rbD.setTextColor(MyContext.getResources().getColor(R.color.black));
+            switch (rgAnswer.getCheckedRadioButtonId()) {
+                case -1:
+                    switch (AnswerList.get(i).text.split(" ")[0]) {
+                        case "(A)":
+                            rbA.setTextColor(MyContext.getResources().getColor(R.color.pink));
+                            break;
+                        case "(B)":
+                            rbB.setTextColor(MyContext.getResources().getColor(R.color.pink));
+                            break;
+                        case "(C)":
+                            rbC.setTextColor(MyContext.getResources().getColor(R.color.pink));
+                            break;
+                        case "(D)":
+                            rbD.setTextColor(MyContext.getResources().getColor(R.color.pink));
+                            break;
+                    }
+                    break;
+                case R.id.rbA:
+                    if (!AnswerList.get(i).text.split(" ")[0].equals("(A)")) {
+                        rbA.setTextColor(MyContext.getResources().getColor(R.color.pink));
+                    } else {
+                        rbA.setTextColor(MyContext.getResources().getColor(R.color.green));
+                    }
+                    break;
+                case R.id.rbB:
+                    if (!AnswerList.get(i).text.split(" ")[0].equals("(B)")) {
+                        rbB.setTextColor(MyContext.getResources().getColor(R.color.pink));
+                    } else {
+                        rbB.setTextColor(MyContext.getResources().getColor(R.color.green));
+                    }
+                    break;
+                case R.id.rbC:
+                    if (!AnswerList.get(i).text.split(" ")[0].equals("(C)")) {
+                        rbC.setTextColor(MyContext.getResources().getColor(R.color.pink));
+                    } else {
+                        rbC.setTextColor(MyContext.getResources().getColor(R.color.green));
+                    }
+                    break;
+                case R.id.rbD:
+                    if (!AnswerList.get(i).text.split(" ")[0].equals("(D)")) {
+                        rbD.setTextColor(MyContext.getResources().getColor(R.color.pink));
+                    } else {
+                        rbD.setTextColor(MyContext.getResources().getColor(R.color.green));
+                    }
+                    break;
+            }
+        }
         return view;
     }
 }
+
